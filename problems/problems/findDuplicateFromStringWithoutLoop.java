947144434698d0ee00aab0537ee40a3030ffc1d2
pavanwakade <pavanwakade199@gmail.com>
2025-12-27 10:07:41 +0530

    Updated

--- problems/problems/findDuplicateFromStringWithoutLoop.java
+++ problems/problems/findDuplicateFromStringWithoutLoop.java
@@ -4,6 +4,6 @@ class findDuplicateFromStringWithoutLoop {
   public static void main(String[] args) {}
 
   public static void duplicate(String str) {
-return str.;
+// return str;
   }
 }
--- problems/problems/reverseString.java
+++ problems/problems/reverseString.java
@@ -1,29 +1,29 @@
-package problems;
+// package problems;
 
-import java.util.Arrays;
+// import java.util.Arrays;
 
-class reverseString {
-  public static void main(String[] args) {
-    System.out.println(reverse("pavan"));
-  }
+// class reverseString {
+//   public static void main(String[] args) {
+//     System.out.println(reverse("pavan"));
+//   }
 
-  public static String reverse(String str) {
-    char[] a = str.toCharArray();
+//   // public static String reverse(String str) {
+//     char[] a = str.toCharArray();
 
-//    System.out.println(Arrays.toString(a));
+// //    System.out.println(Arrays.toString(a));
 
-    String s = "";
+//     String s = "";
 
-    int i = a.length - 1;
+//     int i = a.length - 1;
 
-    for (int j = 0; j < a.length; j++, i--) {
-      s += a[i];
-    }
-//     System.out.println(s);
-//    return s;
+//     for (int j = 0; j < a.length; j++, i--) {
+//       s += a[i];
+//     }
+// //     System.out.println(s);
+// //    return s;
 	  
-	  return new StringBuffer(str
-  }
-}
+// 	  // return new StringBuffer(str
+//   }
+// }
 
 
